4236d034660f2356c91463bd3da22bb3d8a5c29b
Daniyal Aamir <daamir89@gmail.com>
2024-07-14 18:24:51 -0400

    set up webhook to save strip subscription to database

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+import prismadb from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  const body = await request.text()
+  const signature = headers().get("Stripe-Signature") as string
+
+  let event: Stripe.Event
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  } catch(error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session
+
+  if (event.type === "checkout.session.completed") {
+    if (!session.client_reference_id) {
+      return new NextResponse("Client reference id not found", { status: 400 })
+    } 
+
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    await prismadb.subscription.create({
+      data: {
+        userId: session.client_reference_id,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000)
+      }
+    })
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    await prismadb.subscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000)
+      }
+    })
+  } 
+
+  return new NextResponse(null, { status: 200 })
+}
